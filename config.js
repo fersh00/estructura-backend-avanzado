6fbec6dfabb54d6a8d2f7af14e80bae44939b98b
fc <fc323897@gmail.com>
2024-03-07 09:41:18 -0700

    Se modifica config.js

--- config.js
+++ config.js
@@ -5,6 +5,13 @@ dotenv.config();
 
 const connect = () => {
     mongoose.connect(process.env.DB_URI);
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Conexion exitosa a base de datos');
+    });
+    connection.once('error', () => {
+        console.error('Error en conexion a base de datos');
+    }) 
 };
 
 export { connect };
