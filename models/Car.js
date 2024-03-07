91eeecf1974effe0d32ebed186d9d05e632d6989
fc <fc323897@gmail.com>
2024-03-07 16:53:50 -0700

    Se crear archivo Car.js

---
+++ models/Car.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+/**
+ * 1. Crear un schema
+ * 2. Crear modelo, asignando un nombre
+ * 3. 
+ */
+//(1)
+const carSchema = new mongoose.Schema({
+    //Campo --> tipo de dato
+    plate: String,
+    model: String,
+    brand: String,
+    color: String,
+    type: String,
+    carType: String,
+    vin: String
+});
+
+//(2)
+//const Car = mongoose.model('Car', carSchema);
+//export default Car;
+
+export default mongoose.model('Car', carSchema);
